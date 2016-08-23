ab4662127420964eae1e8c3fdd33084e5215f0ee
YaoJingwen <yaojingwen.546@gmail.com>
2016-08-23 12:21:41 -0700

    proton random walk

--- main.m
+++ main.m
@@ -13,21 +13,76 @@ nCube = 3*3*3;
 nFerritinPA = 1; % per aggregate
 nFerritin = nFerritinPA * nAggregate; % per cube
 nProton = 1000;
-time_step = 10^(-9); % s
+time_step = 1*10^(-6); % s
+N = 500; % echo_time2/time_step;
 
 % Calculated parameters
 radius_ferritin = 6*10^(-9); % m
 volume_ferritin = (4/3) * pi * (radius_ferritin)^3; % m^3
 volume_cube = volume_ferritin * nFerritin / volume_fraction; % m^3
 length_cube = nthroot(volume_cube,3); % m
+rmsDisplacement = sqrt(6*diffusion*time_step);
 
 % Aggregate parameters
 radius_aggregate = radius_ferritin;
 
 % Aggregate distribution
 position_aggregate_cube = (rand(nAggregate,3)-1/2).* (length_cube-2*radius_aggregate);
-% scatter3(position_aggregate(:,1),position_aggregate(:,2),position_aggregate(:,3),'b.');
-% axis([-length_cube/2 length_cube/2 -length_cube/2 length_cube/2 -length_cube/2 length_cube/2]);
+position_ferritin = position_aggregate_cube;
+
+scatter3(position_ferritin(:,1),position_ferritin(:,2),position_ferritin(:,3),'b.');
+axis([-length_cube/2 length_cube/2 -length_cube/2 length_cube/2 -length_cube/2 length_cube/2]);
 
 % Random proton intial position
-position_proton(:,1) = rand(3,1).* protonLength - protonLength / 2
+position_proton = zeros(3,N+1);
+distance_ferritin = zeros(nFerritin,N+1);
+while distance_ferritin(:,1) < radius_ferritin % not penetrating
+    position_proton(:,1) = (rand(3,1)-1/2).* length_cube;
+    distance_ferritin(:,1) = sqrt((position_proton(1,1) - position_ferritin(:,1)).^2+...
+            (position_proton(2,1) - position_ferritin(:,2)).^2+...
+            (position_proton(3,1) - position_ferritin(:,3)).^2);
+end
+
+% Random Walk
+random_step = zeros(3,N);
+
+for n = 2:N+1
+    while distance_ferritin(:,n) < radius_ferritin % not penetrating
+        theta = rand * 2 * pi;
+        phi = rand * pi;
+        r = rmsDisplacement;
+        random_step(1,n-1) = r.*sin(theta).*cos(phi);
+        random_step(2,n-1) = r.*sin(theta).*sin(phi);
+        random_step(3,n-1) = r.*cos(theta);
+        position_proton(:,n) = position_proton(:,n-1) + random_step(:,n-1);
+        
+        % Periodic Boundary Conditions
+        if position_proton(1,n) < - length_cube / 2
+            position_proton(1,n) = position_proton(1,n) + length_cube;
+        elseif position_proton(1,n) >  length_cube / 2
+            position_proton(1,n) = position_proton(1,n) - length_cube;
+        end
+        
+        if position_proton(2,n) < - length_cube / 2
+            position_proton(2,n) = position_proton(2,n) + length_cube;
+        elseif position_proton(2,n) >  length_cube / 2
+            position_proton(2,n) = position_proton(2,n) - length_cube;
+        end
+        
+        if position_proton(3,n) < - length_cube / 2
+            position_proton(3,n) = position_proton(3,n) + length_cube;
+        elseif position_proton(3,n) >  length_cube / 2
+            position_proton(3,n) = position_proton(3,n) - length_cube;
+        end
+        
+        distance_ferritin(:,n) = sqrt((position_proton(1,n)-position_ferritin(:,1)).^2+...
+                (position_proton(2,n)-position_ferritin(:,2)).^2+...
+                (position_proton(3,n)-position_ferritin(:,3)).^2);
+    end
+end
+
+hold on
+plot3(position_proton(1,:),position_proton(2,:),position_proton(3,:),'r-')
+hold off
+
+% Calculate grid magnetic field
